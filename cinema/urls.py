a7e1a5479c0fd1776bc08d124c3dc26eddedcdd5
Frezworx <frezworx@gmail.com>
2024-12-21 15:57:33 +0200

    Add API endpoint for managing genres
    Integrated a GenreViewSet with a router and corresponding serializer to handle CRUD operations for genres. Updated URLs to include the new API namespace and route. This provides a clean and RESful way to manage genre-related data.

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,1 +1,11 @@
-# write urls here
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from cinema.views import GenreViewSet
+
+app_name = "cinema"
+router = DefaultRouter()
+router.register("genres", viewset=GenreViewSet, basename="genres")
+urlpatterns = [
+    path("", include(router.urls)),
+]
